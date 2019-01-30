2ff80349359e25b8d410ad17d4d482d7da37cd38
Jeff Setter <setter@stanford.edu>
2019-01-30 14:49:18 -0800

    add new travis files

---
+++ test/scripts/before_install_travis.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+# Needed for new libstdc++ and gcc4.x
+export CXX=${CXX_}
+export CC=${CC_}
+export LLVM_CONFIG=${TRAVIS_BUILD_DIR}/llvm/bin/llvm-config
+export CLANG=${TRAVIS_BUILD_DIR}/llvm/bin/clang
+export COREIRCONFIG="g++-4.9"
+export COREIR_DIR=${TRAVIS_BUILD_DIR}/coreir
+export OUTPUT_REDIRECTION=""
