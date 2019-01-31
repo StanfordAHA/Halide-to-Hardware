ff423be658c56d46624aed63c22a88ff7e4b6c9f
Jeff Setter <setter@stanford.edu>
2019-01-31 10:41:30 -0800

    update how to install llvm (fix url)

--- test/scripts/install_travis.sh
+++ test/scripts/install_travis.sh
@@ -3,5 +3,5 @@
 # Download the right llvm release
 wget https://releases.llvm.org/${LLVM_VERSION}/clang+llvm-${LLVM_VERSION}-linux-x86_64-ubuntu14.04.tar.xz
 tar xf clang+llvm-${LLVM_VERSION}-linux-x86_64-ubuntu14.04.tar.xz
-mv clang+llvm-${LLVM_VERSION}-x86_64-linux-ubuntu-14.04 ${TRAVIS_BUILD_DIR}/llvm
+mv clang+llvm-${LLVM_VERSION}-linux-x86_64-ubuntu14.04 ${TRAVIS_BUILD_DIR}/llvm
 
