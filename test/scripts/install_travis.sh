689b789c8480077584a580d26a8ac76f12efb237
Jeff Setter <setter@stanford.edu>
2019-04-23 23:27:07 -0700

    cleanup travis log

--- test/scripts/install_travis.sh
+++ test/scripts/install_travis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
 # Download the right llvm release
-wget https://releases.llvm.org/${LLVM_VERSION}/clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-ubuntu-14.04.tar.xz
+wget -q https://releases.llvm.org/${LLVM_VERSION}/clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-ubuntu-14.04.tar.xz
 tar xf clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-ubuntu-14.04.tar.xz
 mv clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-ubuntu-14.04 ${TRAVIS_BUILD_DIR}/llvm
