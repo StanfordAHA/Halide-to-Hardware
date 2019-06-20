d06f88c4adec1b327323b25efed8a9d96aff5244
Kathleen Feng <kzf@stanford.edu>
2019-06-19 17:59:56 -0700

    parallel outerprod handcrafted example

--- apps/hardware_benchmarks/tests/handcrafted_ub_outerprod_parallel/process.cpp
+++ apps/hardware_benchmarks/tests/handcrafted_ub_outerprod_parallel/process.cpp
@@ -9,13 +9,16 @@ using namespace Halide::Runtime;
 
 int main(int argc, char **argv) {
 
-  auto input   = Buffer<uint16_t>(8, 256);
-  auto weights = Buffer<uint16_t>(8, 256);
-  auto output  = Buffer<uint16_t>(4, 256);
+  auto input   = Buffer<uint8_t>(4, 4);
+  auto weights = Buffer<uint8_t>(4, 4);
+  auto output  = Buffer<uint8_t>(4, 4);
+
+  auto interleaved = Buffer<uint8_t>(8, 4);
 
   for (int y = 0; y < input.height(); y++) {
     for (int x = 0; x < input.width(); x++) {
       input(x, y) = (1 + x + 2*y) % 70;
+      interleaved(2 * x, y) = input(x, y);
     }
   }
   std::cout << "created input buffer\n";
@@ -23,20 +26,22 @@ int main(int argc, char **argv) {
   for (int y = 0; y < weights.height(); y++) {
     for (int x = 0; x < weights.width(); x++) {
       weights(x, y) = (1 + x + y);
+      interleaved(2 * x + 1, y) = weights(x, y);
     }
   }
   std::cout << "created weights buffer\n";
-    
+
+  // weights are transposed
   for (int y = 0; y < output.height(); y++) {
-    output(0, y) = input(0, y) * weights(0, y) + input(1, y) * weights(1, y) + input(2, y) * weights(2, y) + input(3, y) * weights(3, y);
-    output(1, y) = input(0, y) * weights(4, y) + input(1, y) * weights(5, y) + input(2, y) * weights(6, y) + input(3, y) * weights(7, y);
-    output(2, y) = input(4, y) * weights(0, y) + input(5, y) * weights(1, y) + input(6, y) * weights(2, y) + input(7, y) * weights(3, y);
-    output(3, y) = input(5, y) * weights(4, y) + input(5, y) * weights(5, y) + input(6, y) * weights(6, y) + input(7, y) * weights(7, y);
+      for (int x = 0; x < output.width(); x++) {
+          output(x, y) = input(x, 0) * weights(y, 0) + input(x, 1) * weights(y, 1) + input(x, 2) * weights(y, 2) + input(x, 3) * weights(y, 3);
+      }
   }
   std::cout << "created output buffer\n";
 
-  save_image(input,   "bin/input.png");
-  save_image(weights, "bin/weights.png");
+  //save_image(input,   "bin/input.png");
+  save_image(weights, "weights.png");
   save_image(output,  "bin/output_cpu.png");
+  save_image(interleaved, "input.png");
   return 0;
 }
