a4013f500e3a803ee9a6a395df2b606f760b27a1
Kathleen Feng <kzf@stanford.edu>
2019-06-20 15:30:25 -0700

    add downsample

---
+++ apps/hardware_benchmarks/tests/handcrafted_ub_maxpool/process.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+
+#include "hardware_process_helper.h"
+#include "coreir_interpret.h"
+#include "halide_image_io.h"
+
+using namespace Halide::Tools;
+using namespace Halide::Runtime;
+
+int main(int argc, char **argv) {
+
+  auto input   = Buffer<uint8_t>(16, 16);
+  auto output  = Buffer<uint8_t>(8, 8);
+
+  for (int y = 0; y < input.height(); y++) {
+    for (int x = 0; x < input.width(); x++) {
+      input(x, y) = (1 + x + 2*y) % 70;
+    }
+  }
+  std::cout << "created input buffer\n";
+  
+  for (int y = 0; y < output.height(); y++) {
+      for (int x = 0; x < output.width(); x++) {
+          output(x,y) = std::max(std::max(input(x,y), input(x+1,y)), std::max(input(x,y+1),input(x+1,y+1)));
+      }
+  }
+  std::cout << "created output buffer\n";
+
+  save_image(output,  "bin/output_cpu.png");
+  save_image(input, "input.png");
+  return 0;
+}
