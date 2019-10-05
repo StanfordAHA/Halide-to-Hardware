557e8056688e218762a69363b45d4671acc4bbe0
Dillon Huff <dillonhuff@gmail.com>
2019-10-04 17:30:56 -0700

    Want to compile to coreir, but nothing is happening?

--- apps/hardware_benchmarks/tests/merged_unit_tests/test_main.cpp
+++ apps/hardware_benchmarks/tests/merged_unit_tests/test_main.cpp
@@ -10,14 +10,30 @@ using namespace Halide::Tools;
 using namespace std;
 
 void pointwise_test() {
-  Halide::Buffer<uint16_t> in(4, 4);
 
-  Var x, y;
-  Halide::Func gradient;
-  gradient(x, y) = in(x, y) + 10;
+    Func brighter;
+    Var x, y;
+    Var xo, yo, xi, yi;
 
-  Halide::Buffer<uint16_t> out(4, 4);
-  out = gradient.realize(out.width(), out.height());;
+    ImageParam input(type_of<uint8_t>(), 2);
+
+    brighter(x, y) = input(x, y) + 10;
+
+    brighter.compile_to_static_library("cpu_brighter", {input}, "brighter");
+
+    brighter.tile(x, y, xo, yo, xi, yi, 2, 2).hw_accelerate(xi, yi);
+    brighter.compile_to_coreir("coreir_brighter", {input}, "brighter");
+  //Input<Buffer<uint8_t>>  input{"input", 2};
+  //Output<Buffer<uint8_t>> output{"output", 2};
+  
+  //Var x, y;
+  //Halide::Func gradient;
+  //gradient(x, y) = input(x, y) + 10;
+
+  //Halide::Buffer<uint16_t> in(4, 4);
+
+  //Halide::Buffer<uint16_t> out(4, 4);
+  //out = gradient.realize(out.width(), out.height());;
 }
 
 int main(int argc, char **argv) {
