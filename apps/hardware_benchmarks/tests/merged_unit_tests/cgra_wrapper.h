8399e438b1d782476b5d08db6f9900d86db18edd
Dillon Huff <dillonhuff@gmail.com>
2019-10-10 13:41:35 -0700

    Now have simple example of demosaic cpp output compiling, but I need to insert the correct hardware machinery to get it to really run

--- apps/hardware_benchmarks/tests/merged_unit_tests/cgra_wrapper.h
+++ apps/hardware_benchmarks/tests/merged_unit_tests/cgra_wrapper.h
@@ -3,40 +3,48 @@
 // p
 //
 void halide_error_bad_type(void *user_context, const char*) {
-
+  assert(false);
 }
 
 int halide_error_explicit_bounds_too_small(void *user_context, const char *func_name, const char *var_name,
                                                       int min_bound, int max_bound, int min_required, int max_required) {
+  assert(false);
   return 0;
 }
 
 int halide_error_bad_type(void *user_context, const char *func_name,
                                  uint32_t type_given, uint32_t correct_type)// N.B. The last two args are the bit representation of a halide_type_t {
 {
-  return 0;
+  std::cerr << "Expected type " << correct_type << ", but got: " << type_given << std::endl;
+  return -1;
 }
 
 int halide_error_bad_dimensions(void *user_context, const char *func_name,
                                        int32_t dimensions_given, int32_t correct_dimensions) {
+  std::cerr << "Error (Bad Dimensions): In " << func_name << ", expecting " << correct_dimensions << ", but got " << dimensions_given << std::endl;
+  assert(false);
   return 0;
 }
 
 int halide_error_access_out_of_bounds(void *user_context, const char *func_name,
                                              int dimension, int min_touched, int max_touched,
                                              int min_valid, int max_valid) {
+  assert(false);
   return 0;
 }
 int halide_error_buffer_allocation_too_large(void *user_context, const char *buffer_name,
                                                     uint64_t allocation_size, uint64_t max_size) {
+  assert(false);
   return 0;
 }
 int halide_error_buffer_extents_negative(void *user_context, const char *buffer_name, int dimension, int extent) {
+  assert(false);
   return 0;
 }
 
 int halide_error_buffer_extents_too_large(void *user_context, const char *buffer_name,
                                                  int64_t actual_size, int64_t max_size) {
+  assert(false);
   return 0;
 }
 //extern int halide_error_constraints_make_required_region_smaller(void *user_context, const char *buffer_name,
@@ -45,6 +53,7 @@ int halide_error_buffer_extents_too_large(void *user_context, const char *buffer
                                                                  //int required_min, int required_extent);
 int halide_error_constraint_violated(void *user_context, const char *var, int val,
                                             const char *constrained_var, int constrained_val) {
+  assert(false);
   return 0;
 }
 //extern int halide_error_param_too_small_i64(void *user_context, const char *param_name,
@@ -60,15 +69,18 @@ int halide_error_constraint_violated(void *user_context, const char *var, int va
 //extern int halide_error_param_too_large_f64(void *user_context, const char *param_name,
                                             //double val, double max_val);
 int halide_error_out_of_memory(void *user_context) {
+  assert(false);
   return 0;
 }
 int halide_error_buffer_argument_is_null(void *user_context, const char *buffer_name) {
+  assert(false);
   return 0;
 }
 //extern int halide_error_debug_to_file_failed(void *user_context, const char *func,
                                              //const char *filename, int error_code);
 //extern int halide_error_unaligned_host_ptr(void *user_context, const char *func_name, int alignment);
 int halide_error_host_is_null(void *user_context, const char *func_name) {
+  assert(false);
   return 0;
 }
 //extern int halide_error_failed_to_upgrade_buffer_t(void *user_context,
---
+++ apps/hardware_benchmarks/tests/merged_unit_tests/demosaic_soc_run.cpp
@@ -0,0 +1,21 @@
+#include "demosaic_soc_mini.cpp"
+
+#include <iostream>
+
+#include "HalideBuffer.h"
+
+using namespace std;
+
+// g++ lesson_10*run.cpp lesson_10_halide.a -o lesson_10_run -I ../include
+int main() {
+  //halide_buffer_t input;
+  //halide_buffer_t output;
+
+  Halide::Runtime::Buffer<uint16_t> input(10, 10), output(8, 8, 3);
+  cout << "Starting demosaic" << endl;
+  hw_demosaic(input, output);
+  //hw_demosaic(&input, &output);
+
+  cout << "Done" << endl;
+  return 0;
+}
