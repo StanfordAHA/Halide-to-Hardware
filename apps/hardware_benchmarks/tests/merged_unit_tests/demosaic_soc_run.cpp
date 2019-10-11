b55c78a83069cc7415f9b7ab5f480e83f052aeed
Dillon Huff <dillonhuff@gmail.com>
2019-10-11 14:42:24 -0700

    Input host location is not the same in cpp driver and output?

--- apps/hardware_benchmarks/tests/merged_unit_tests/demosaic_soc_run.cpp
+++ apps/hardware_benchmarks/tests/merged_unit_tests/demosaic_soc_run.cpp
@@ -12,8 +12,23 @@ int main() {
   //halide_buffer_t output;
 
   //Halide::Runtime::Buffer<uint16_t> input(10, 10), output(8, 8, 3);
-  Halide::Runtime::Buffer<uint16_t> input(200, 200), output(200 - 8, 200 - 8, 3);
-  cout << "Starting demosaic" << endl;
+  //Halide::Runtime::Buffer<uint16_t> input(200, 200), output(200 - 8, 200 - 8, 3);
+  Halide::Runtime::Buffer<uint16_t> input(200, 200), output(10, 10, 3);
+  for (int y = 0; y < input.height(); y++) {
+    for (int x = 0; x < input.width(); x++) {
+      input(x, y) = 23;
+    }
+  }
+
+  cout << "After setting all inputs to 23..." << endl;
+  halide_buffer_t* inputBuf = input;
+  void* inputHost = inputBuf->host;
+  printf("Input Buffer location = %lld\n", (unsigned long long ) inputBuf);
+  printf("Input host location = %lld\n", (unsigned long long ) &inputHost);
+  for (int i = 0; i < 10; i++) {
+    printf("input[%d] = %d\n", i, ((uint16_t*)(inputBuf->host))[i]);
+  }
+  cout << "Starting demosaic with input set to all 23" << endl;
   hw_demosaic(input, output);
   //hw_demosaic(&input, &output);
 
