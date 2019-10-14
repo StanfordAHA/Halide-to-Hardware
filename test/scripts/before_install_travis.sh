20d1e7cc8887288f60ca932e307ed0e5f6cf73d6
Dillon Huff <dillonhuff@gmail.com>
2019-10-14 10:52:34 -0700

    Changed coreir gcc to explicitly set to 4.9

--- test/scripts/before_install_travis.sh
+++ test/scripts/before_install_travis.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -x
 
 # Needed for new libstdc++ and gcc4.x
-export CXX=g++
-export CC=gcc
+export CXX=g++-4.9
+export CC=gcc-4.9
 export LLVM_CONFIG=/usr/local/opt/llvm/lib/cmake/llvm/
 export CLANG=/usr/local/opt/llvm/bin/
-export COREIRCONFIG="g++"
+export COREIRCONFIG="g++-4.9"
 export COREIR_DIR=/Users/dillon/CppWorkspace/coreir/
 export OUTPUT_REDIRECTION=""
 export HALIDE_SHARED_LIBARY="halide.a"
