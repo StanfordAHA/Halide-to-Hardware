d4a1f22a0463852bf5148966734352f46ead9c85
Dillon Huff <dillonhuff@gmail.com>
2019-10-17 15:27:13 -0700

    Generalizing cgrawrapper to use different bits

--- apps/hardware_benchmarks/tests/merged_unit_tests/cgra_wrapper.cpp
+++ apps/hardware_benchmarks/tests/merged_unit_tests/cgra_wrapper.cpp
@@ -15,6 +15,7 @@ using namespace std;
 
 extern "C" {
 uint8_t* _halide_buffer_get_host(const halide_buffer_t* buf);
+uint32_t _halide_buffer_get_type(const halide_buffer_t *buf);
 }
 
 bool starts_with(const string &str, const string &prefix) {
@@ -97,63 +98,6 @@ void CGRAWrapper::subimage_to_stream(halide_buffer_t* buf, int32_t subImageOffse
     int stride_1, int subimage_extent_1,
     int stride_2, int subimage_extent_2,
     int stride_3, int subimage_extent_3) {
-
-  //cout << "subimage to stream on buffer at offset " << subImageOffset << " with extents..." << endl;
-  //cout << "\t" << subimage_extent_0 << endl;
-  //cout << "\t" << subimage_extent_1 << endl;
-  //cout << "\t" << subimage_extent_2 << endl;
-  //cout << "\t" << subimage_extent_3 << endl;
-
-  //assert(stride_3 == 1 && subimage_extent_3 == 1);
-
-  //CoordinateVector<int> writeIdx({"y", "x", "c"}, {subimage_extent_1 - 1,
-      //subimage_extent_0 - 1,
-      //subimage_extent_2 - 1});
-
-  //pixelOutputs.clear();
-  //assert(pixelOutputs.size() == 0);
-
-  //uint16_t* hostBuf = (uint16_t*) _halide_buffer_get_host(buf);
-  //while (!writeIdx.allDone()) {
-    //cout << "Write index = " << writeIdx.coordString() << endl;
-    //auto i = writeIdx.coord("x");
-    //auto j = writeIdx.coord("y");
-    //auto k = writeIdx.coord("c");
-    //// TODO: Replace with 4th dimension stride when that comes up
-    //auto m = 1;
-
-    //int offset = subImageOffset + 
-      //stride_0 * i +
-      //stride_1 * j + 
-      //stride_2 * k + 
-      //stride_3 * m;
-    //uint16_t nextInPixel = hostBuf[offset];
-
-    //string input_name = "self.in_arg_0_0_0";
-    //cout << "Next pixel = " << nextInPixel << endl;
-    //state->setValue("self.in_en", BitVector(1, true));
-    //state->setValue(input_name, BitVector(16, nextInPixel));
-    //state->exeCombinational();
-
-
-    //bool valid_value = state->getBitVec("self.valid").to_type<bool>();
-    //if (valid_value) {
-
-      //auto output_bv = state->getBitVec("self.out_0_0");
-      //int output_value;
-      //output_value = output_bv.to_type<int>();
-      //cout << "\tOutput value = " << output_value << endl;
-    //}
-    
-    //pixelOutputs.push_back(23);
-    //pixelOutputs.push_back(23);
-    //pixelOutputs.push_back(23);
-
-    //state->exeSequential();
-
-    //writeIdx.increment();
-  //}
-
 }
 
 void CGRAWrapper::stream_to_subimage(halide_buffer_t* buf, int32_t subImageOffset,
@@ -161,35 +105,6 @@ void CGRAWrapper::stream_to_subimage(halide_buffer_t* buf, int32_t subImageOffse
     int stride_1, int subimage_extent_1,
     int stride_2, int subimage_extent_2,
     int stride_3, int subimage_extent_3) {
-
-  //cout << "subimage to stream on buffer with extents..." << endl;
-  //cout << "\t" << subimage_extent_0 << endl;
-  //cout << "\t" << subimage_extent_1 << endl;
-  //cout << "\t" << subimage_extent_2 << endl;
-  //cout << "\t" << subimage_extent_3 << endl;
-  
-  //uint16_t* dataPtr = (uint16_t*)_halide_buffer_get_host(buf);
-  //int nextPix = 0;
-  //for (int i = 0; i < subimage_extent_0; i++) {
-    //for (int j = 0; j < subimage_extent_1; j++) {
-      //for (int k = 0; k < subimage_extent_2; k++) {
-        //for (int m = 0; m < subimage_extent_3; m++) {
-          //int offset = subImageOffset + 
-            //stride_0 * i +
-            //stride_1 * j + 
-            //stride_2 * k + 
-            //stride_3 * m;
-          //assert(nextPix < ((int) pixelOutputs.size()));
-          //dataPtr[offset] = pixelOutputs[nextPix];
-          //nextPix++;
-        //}
-      //}
-    //}
-  //}
-
-  //cout << "nextPix = " << nextPix << endl;
-  //cout << "pixels  = " << pixelOutputs.size() << endl;
-  ////assert(nextPix == ((int) pixelOutputs.size()));
 }
 
 void CGRAWrapper::produce_subimage(halide_buffer_t* sourceBuf, int32_t sourceOffset,
@@ -207,6 +122,19 @@ void CGRAWrapper::produce_subimage(halide_buffer_t* sourceBuf, int32_t sourceOff
   typedef uint8_t InElemType;
   typedef uint8_t OutElemType;
 
+  uint32_t sourceElemType = _halide_buffer_get_type(sourceBuf);
+  uint32_t destElemType = _halide_buffer_get_type(destBuf);
+
+  cout << "Source type = " << hex << sourceElemType << endl;
+  cout << "Dest type   = " << hex << destElemType << endl;
+
+  uint32_t sourceBits = (sourceElemType >> 8) & 0xff;
+  uint32_t destBits = (destElemType >> 8) & 0xff;
+ 
+  cout << dec << endl;
+  cout << "Source bits = " << sourceBits << endl;
+  cout << "Dest bits   = " << destBits << endl;
+  
   cout << "subimage to stream on buffer at offset " << destOffset << " with extents..." << endl;
   cout << "\t" << dest_subimage_extent_0 << endl;
   cout << "\t" << dest_subimage_extent_1 << endl;
@@ -235,7 +163,6 @@ void CGRAWrapper::produce_subimage(halide_buffer_t* sourceBuf, int32_t sourceOff
   pixelOutputs.clear();
   assert(pixelOutputs.size() == 0);
 
-  InElemType* hostBuf = (InElemType*) _halide_buffer_get_host(sourceBuf);
   while (!readIdx.allDone()) {
     cout << "Write index = " << writeIdx.coordString() << endl;
     cout << "Read index  = " << readIdx.coordString() << endl;
@@ -252,10 +179,25 @@ void CGRAWrapper::produce_subimage(halide_buffer_t* sourceBuf, int32_t sourceOff
       src_stride_3 * m;
 
     if (!writeIdx.allDone()) {
-      InElemType nextInPixel = hostBuf[offset];
-      cout << "Next pixel = " << nextInPixel << endl;
-      state->setValue("self.in_en", BitVector(1, true));
-      state->setValue(inputName, BitVector(16, nextInPixel));
+
+      assert(sourceBits == 8 || sourceBits == 16);
+
+      if (sourceBits == 8) {
+        uint8_t* hostBuf = (uint8_t*) _halide_buffer_get_host(sourceBuf);
+        uint8_t nextInPixel = hostBuf[offset];
+        cout << "Next pixel = " << nextInPixel << endl;
+        state->setValue("self.in_en", BitVector(1, true));
+        // Note: Accelerator promotes everything to 16 bits on input
+        state->setValue(inputName, BitVector(16, nextInPixel));
+      } else {
+        assert(sourceBits == 16);
+
+        uint16_t* hostBuf = (uint16_t*) _halide_buffer_get_host(sourceBuf);
+        uint16_t nextInPixel = hostBuf[offset];
+        cout << "Next pixel = " << nextInPixel << endl;
+        state->setValue("self.in_en", BitVector(1, true));
+        state->setValue(inputName, BitVector(16, nextInPixel));
+      }
     } else {
       state->setValue("self.in_en", BitVector(1, false));
     }
