6c39e01de2f6bd9dc7625478e5511513fe2e0ace
Dillon Huff <dillonhuff@gmail.com>
2019-11-01 19:58:31 -0700

    Changed gcc to 5 instead of 7 to see if that changes anything

--- test/scripts/before_install_travis.sh
+++ test/scripts/before_install_travis.sh
@@ -6,6 +6,6 @@ export CC=${CC_}
 export LLVM_CONFIG=${TRAVIS_BUILD_DIR}/llvm/bin/llvm-config
 export CLANG=${TRAVIS_BUILD_DIR}/llvm/bin/clang
 #export COREIRCONFIG="g++-4.9"
-export COREIRCONFIG="g++-7"
+export COREIRCONFIG="g++-5"
 export COREIR_DIR=${TRAVIS_BUILD_DIR}/coreir
 export OUTPUT_REDIRECTION=""
