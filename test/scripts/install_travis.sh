a66a02678ad3b2acb3cacfd4aa954372cd27a702
Dillon Huff <dillonhuff@gmail.com>
2019-11-07 16:59:25 -0800

    Changed llvm download

--- test/scripts/install_travis.sh
+++ test/scripts/install_travis.sh
@@ -1,18 +1,21 @@
 #!/bin/bash -x
 
-# Download the right llvm release
+wget -q http://releases.llvm.org/${LLVM_VERSION}/clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-ubuntu-14.04.tar.xz
+tar xf clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-ubuntu-14.04.tar.xz
+mv clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-ubuntu-14.04 ${TRAVIS_BUILD_DIR}/llvm
+## Download the right llvm release
 
-LLVM_VERSION_NUM=`$LLVM_CONFIG --version | cut -b 1`
-if [[ ${LLVM_VERSION_NUM} -gt "5" ]]
-then
-    wget -q http://releases.llvm.org/${LLVM_VERSION}/clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-ubuntu-14.04.tar.xz
-    tar xf clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-ubuntu-14.04.tar.xz
-    mv clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-ubuntu-14.04 ${TRAVIS_BUILD_DIR}/llvm
-else
-    wget -q https://releases.llvm.org/${LLVM_VERSION}/clang+llvm-${LLVM_VERSION}-linux-x86_64-ubuntu14.04.tar.xz
-    tar xf clang+llvm-${LLVM_VERSION}-linux-x86_64-ubuntu14.04.tar.xz
-    mv clang+llvm-${LLVM_VERSION}-linux-x86_64-ubuntu14.04 ${TRAVIS_BUILD_DIR}/llvm
-fi
+#LLVM_VERSION_NUM=`$LLVM_CONFIG --version | cut -b 1`
+#if [[ ${LLVM_VERSION_NUM} -gt "5" ]]
+#then
+    #wget -q http://releases.llvm.org/${LLVM_VERSION}/clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-ubuntu-14.04.tar.xz
+    #tar xf clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-ubuntu-14.04.tar.xz
+    #mv clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-ubuntu-14.04 ${TRAVIS_BUILD_DIR}/llvm
+#else
+    #wget -q https://releases.llvm.org/${LLVM_VERSION}/clang+llvm-${LLVM_VERSION}-linux-x86_64-ubuntu14.04.tar.xz
+    #tar xf clang+llvm-${LLVM_VERSION}-linux-x86_64-ubuntu14.04.tar.xz
+    #mv clang+llvm-${LLVM_VERSION}-linux-x86_64-ubuntu14.04 ${TRAVIS_BUILD_DIR}/llvm
+#fi
 
 # compile coreir
 git clone -b ubuffer https://github.com/rdaly525/coreir.git
