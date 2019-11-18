8f3325e0ec9ed4e34b3a4abd806983ff253fd0f6
Dillon Huff <dillonhuff@gmail.com>
2019-11-17 21:28:16 -0800

    Fixed command syntax

--- test/scripts/garnet_docker_build.sh
+++ test/scripts/garnet_docker_build.sh
@@ -8,7 +8,7 @@ docker pull keyiz/garnet-flow-cad
 docker run -itd --name halide-hw-distro keyiz/garnet-flow-cad bash
 # TODO: Change this script to use travis environment variables
 docker exec -i halide-hw-distro bash -c "git clone --branch dhuff_migrate_coreir_build_halide_from_scratch --depth 1 https://github.com/StanfordAHA/GarnetFlow.git"
-eval (CMD_STRING)
+eval $CMD_STRING
 #docker exec -i halide-hw-distro bash -c "cd GarnetFlow/scripts/; ./build_halide_distrib.sh StanfordAHA/Halide-to-Hardware $COMMIT"
 # StanfordAHA/Halide-to-Hardware hls_hwbuffer_codegen_garnetflow_updates"
 docker exec -i halide-hw-distro bash -c "cd GarnetFlow/scripts/Halide-to-Hardware; ./test/scripts/compare_app.sh pointwise"
