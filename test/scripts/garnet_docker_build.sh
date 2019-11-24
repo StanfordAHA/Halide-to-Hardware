848d6c9b893d74cf0b33520479cf44e241c7bdb2
Dillon Huff <dillonhuff@gmail.com>
2019-11-24 12:56:04 +0100

    Adding docker exit check

--- test/scripts/garnet_docker_build.sh
+++ test/scripts/garnet_docker_build.sh
@@ -1,20 +1,18 @@
-#TRAVIS_COMMIT=asgfdsgadsf
-CMD_STRING="docker exec -i halide-hw-distro bash -c \"cd GarnetFlow/scripts; ./build_halide_distrib.sh StanfordAHA/Halide-to-Hardware $TRAVIS_COMMIT\""
 
-echo "CMD_STRING is..."
-echo $CMD_STRING
-#exit
 docker pull keyiz/garnet-flow-cad
 docker run -itd --name halide-hw-distro keyiz/garnet-flow-cad bash
-# TODO: Change this script to use travis environment variables
-docker exec -i halide-hw-distro bash -c "git clone --branch dhuff_migrate_coreir_build_halide_from_scratch --depth 1 https://github.com/StanfordAHA/GarnetFlow.git"
+docker exec -i halide-hw-distro bash -c "git clone --branch dhuff_migrate_coreir_build_halide_from_scratch --depth 1 https://github.com/StanfordAHA/GarnetFlow.git; exit $?"
+
+# Pull the commit we want from github and build it
+CMD_STRING="docker exec -i halide-hw-distro bash -c \"cd GarnetFlow/scripts; ./build_halide_distrib.sh StanfordAHA/Halide-to-Hardware $TRAVIS_COMMIT\"; exit $?"
+echo "CMD_STRING is..."
+echo $CMD_STRING
 eval $CMD_STRING
-#docker exec -i halide-hw-distro bash -c "cd GarnetFlow/scripts/; ./build_halide_distrib.sh StanfordAHA/Halide-to-Hardware $COMMIT"
-# StanfordAHA/Halide-to-Hardware hls_hwbuffer_codegen_garnetflow_updates"
-docker exec -i halide-hw-distro bash -c "cd GarnetFlow/scripts/Halide-to-Hardware; ./test/scripts/compare_app.sh pointwise"
+
+docker exec -i halide-hw-distro bash -c "cd GarnetFlow/scripts/Halide-to-Hardware; ./test/scripts/compare_app.sh pointwise; exit $?"
 echo 'Pointwise result?'
 echo $?
-docker exec -i halide-hw-distro bash -c "cd GarnetFlow/scripts/Halide-to-Hardware; ./test/scripts/compare_test.sh conv_3_3"
+docker exec -i halide-hw-distro bash -c "cd GarnetFlow/scripts/Halide-to-Hardware; ./test/scripts/compare_test.sh conv_3_3; exit $?"
 echo 'Conv result?'
 echo $?
 docker cp halide-hw-distro:/GarnetFlow/scripts/Halide-to-Hardware/halide_distrib.tgz ./halide_distrib.tgz
