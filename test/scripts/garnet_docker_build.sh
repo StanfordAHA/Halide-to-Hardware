8e96aae220d9f057913c646d0546b350b801bab6
Dillon Huff <dillonhuff@gmail.com>
2019-11-24 12:32:31 +0100

    Checking process outputs

--- test/scripts/garnet_docker_build.sh
+++ test/scripts/garnet_docker_build.sh
@@ -12,6 +12,10 @@ eval $CMD_STRING
 #docker exec -i halide-hw-distro bash -c "cd GarnetFlow/scripts/; ./build_halide_distrib.sh StanfordAHA/Halide-to-Hardware $COMMIT"
 # StanfordAHA/Halide-to-Hardware hls_hwbuffer_codegen_garnetflow_updates"
 docker exec -i halide-hw-distro bash -c "cd GarnetFlow/scripts/Halide-to-Hardware; ./test/scripts/compare_app.sh pointwise"
+echo 'Pointwise result?'
+echo $?
 docker exec -i halide-hw-distro bash -c "cd GarnetFlow/scripts/Halide-to-Hardware; ./test/scripts/compare_test.sh conv_3_3"
+echo 'Conv result?'
+echo $?
 docker cp halide-hw-distro:/GarnetFlow/scripts/Halide-to-Hardware/halide_distrib.tgz ./halide_distrib.tgz
 
