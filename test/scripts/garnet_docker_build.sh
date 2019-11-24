af2edaffc3a44bd2c50dffe7a1d42dfbc96c07d0
Dillon Huff <dillonhuff@gmail.com>
2019-11-24 14:49:34 +0100

    Need to add result script

--- test/scripts/garnet_docker_build.sh
+++ test/scripts/garnet_docker_build.sh
@@ -13,21 +13,21 @@ echo $CMD_STRING
 eval $CMD_STRING
 
 echo 'Running pointwise...'
-docker exec -i halide-hw-distro bash -c "cd GarnetFlow/scripts/Halide-to-Hardware; ./test/scripts/compare_app.sh pointwise >& pointwise_res.txt"
-docker cp halide-hw-distro:/GarnetFlow/scripts/Halide-to-Hardware/pointwise_res.txt ./pointwise_res.txt
-echo 'pointwise_res.txt...'
-cat pointwise_res.txt
-awk '/./{line=$0} END{print line}' pointwise_res.txt > last_line.txt
-pw_res=`cat last_line.txt`
-echo 'Last line...'
-echo $pw_res
-if [ $pw_res = "Images are equivalent!" ]
-then
-  echo 'Test passed...'
-else
-  echo 'Pointwise FAILED'
-  exit 1
-fi
+docker exec -i halide-hw-distro bash -c "cd GarnetFlow/scripts/Halide-to-Hardware; ./test/scripts/compare_app.sh pointwise; echo $?; echo \"done with pointwise\""
+#docker cp halide-hw-distro:/GarnetFlow/scripts/Halide-to-Hardware/pointwise_res.txt ./pointwise_res.txt
+#echo 'pointwise_res.txt...'
+#cat pointwise_res.txt
+#awk '/./{line=$0} END{print line}' pointwise_res.txt > last_line.txt
+#pw_res=`cat last_line.txt`
+#echo 'Last line...'
+#echo $pw_res
+#if [ $pw_res = "Images are equivalent!" ]
+#then
+  #echo 'Test passed...'
+#else
+  #echo 'Pointwise FAILED'
+  #exit 1
+#fi
 
 echo 'Pointwise result?'
 echo $?
