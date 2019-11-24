98eb1248b67c63a370d9e58f00893b07eaf5ab5d
Dillon Huff <dillonhuff@gmail.com>
2019-11-24 13:17:25 +0100

    Added more printouts to build script

--- test/scripts/garnet_docker_build.sh
+++ test/scripts/garnet_docker_build.sh
@@ -1,19 +1,28 @@
 
+echo 'Pulling docker image'
 docker pull keyiz/garnet-flow-cad
+echo 'Running docker container'
 docker run -itd --name halide-hw-distro keyiz/garnet-flow-cad bash
-docker exec -i halide-hw-distro bash -c "git clone --branch dhuff_migrate_coreir_build_halide_from_scratch --depth 1 https://github.com/StanfordAHA/GarnetFlow.git; exit $?"
+echo 'Cloning GarnetFlow into container'
+docker exec -i halide-hw-distro bash -c "git clone --branch dhuff_migrate_coreir_build_halide_from_scratch --depth 1 https://github.com/StanfordAHA/GarnetFlow.git;"
 
+echo 'Running Halide build...'
 # Pull the commit we want from github and build it
-CMD_STRING="docker exec -i halide-hw-distro bash -c \"cd GarnetFlow/scripts; ./build_halide_distrib.sh StanfordAHA/Halide-to-Hardware $TRAVIS_COMMIT\"; exit $?"
+CMD_STRING="docker exec -i halide-hw-distro bash -c \"cd GarnetFlow/scripts; ./build_halide_distrib.sh StanfordAHA/Halide-to-Hardware $TRAVIS_COMMIT\";"
 echo "CMD_STRING is..."
 echo $CMD_STRING
 eval $CMD_STRING
 
-docker exec -i halide-hw-distro bash -c "cd GarnetFlow/scripts/Halide-to-Hardware; ./test/scripts/compare_app.sh pointwise; exit $?"
+echo 'Running pointwise...'
+docker exec -i halide-hw-distro bash -c "cd GarnetFlow/scripts/Halide-to-Hardware; ./test/scripts/compare_app.sh pointwise;"
 echo 'Pointwise result?'
 echo $?
-docker exec -i halide-hw-distro bash -c "cd GarnetFlow/scripts/Halide-to-Hardware; ./test/scripts/compare_test.sh conv_3_3; exit $?"
+
+echo 'Running conv_3_3...'
+docker exec -i halide-hw-distro bash -c "cd GarnetFlow/scripts/Halide-to-Hardware; ./test/scripts/compare_test.sh conv_3_3;"
 echo 'Conv result?'
 echo $?
+
+echo 'Copying built distribution to travis'
 docker cp halide-hw-distro:/GarnetFlow/scripts/Halide-to-Hardware/halide_distrib.tgz ./halide_distrib.tgz
 
