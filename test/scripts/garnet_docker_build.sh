114d307e5a1fc64cbda553c56df652a6dd78c700
Dillon Huff <dillonhuff@gmail.com>
2019-11-24 15:39:47 +0100

    Fixed misspelling in testbench

--- test/scripts/garnet_docker_build.sh
+++ test/scripts/garnet_docker_build.sh
@@ -15,7 +15,7 @@ eval $CMD_STRING
 # Run the testbench in docker, copy the result to the local travis machine, and
 # then stop the script if the testbench failed.
 echo 'Running testbench...'
-docker exec -i halide-hw-distro bach -c "cd /GarnetFlow/scripts/Halide-to-Hardware; ./test/scripts/garnet_docker_testbench.sh"
+docker exec -i halide-hw-distro bash -c "cd /GarnetFlow/scripts/Halide-to-Hardware; ./test/scripts/garnet_docker_testbench.sh"
 echo 'Extracting testbench files...'
 docker cp halide-hw-distro:/GarnetFlow/scripts/Halide-to-Hardware/tb_res.txt ./tb_res.txt
 test_res=`cat tb_res.txt`
