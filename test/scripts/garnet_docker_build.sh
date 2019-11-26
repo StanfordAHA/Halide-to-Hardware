42e2e9068ce72646668d9f0521ad7038cc35aa06
Dillon Huff <dillonhuff@gmail.com>
2019-11-26 07:35:21 +0100

    Restored old install travis

--- test/scripts/garnet_docker_build.sh
+++ test/scripts/garnet_docker_build.sh
@@ -3,12 +3,10 @@ docker pull keyiz/garnet-flow-cad
 echo 'Running docker container'
 docker run -itd --name halide-hw-distro keyiz/garnet-flow-cad bash
 echo 'Cloning GarnetFlow into container'
-#docker exec -i halide-hw-distro bash -c "git clone --branch dhuff_migrate_coreir_build_halide_from_scratch --depth 1 https://github.com/StanfordAHA/GarnetFlow.git;"
 docker exec -i halide-hw-distro bash -c "git clone https://github.com/StanfordAHA/GarnetFlow.git;"
 
 echo 'Running Halide build...'
 # Pull the commit we want from github and build it
-#CMD_STRING="docker exec -i halide-hw-distro bash -c \"cd GarnetFlow/scripts; ./build_halide_distrib.sh StanfordAHA/Halide-to-Hardware $TRAVIS_COMMIT\";"
 CMD_STRING="docker exec -i halide-hw-distro bash -c \"cd GarnetFlow/scripts; ./build_halide_distrib.sh $TRAVIS_REPO_SLUG $TRAVIS_COMMIT\";"
 echo "CMD_STRING is..."
 echo $CMD_STRING
