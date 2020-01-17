c40781ad3566f5b60cb90c471f0ab85eb6e60036
Dillon Huff <dillonhuff@gmail.com>
2020-01-16 23:12:54 -0800

    Setting ld lib path

--- test/scripts/run_hw_unit_tests.sh
+++ test/scripts/run_hw_unit_tests.sh
@@ -1,7 +1,11 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
   echo "On mac"
   source ./test/scripts/mac_settings.sh
+else
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./bin/
 fi
+
+echo 'LD_LIBRARY_PATH =' $LD_LIBRARY_PATH
 cd ./apps/hardware_benchmarks/tests/merged_unit_tests/
 if make all; then
   echo "Make finished"
