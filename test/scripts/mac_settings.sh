fc7367378d8b40c14bb1042f92ec8b958f8b5b63
Dillon Huff <dillonhuff@gmail.com>
2020-02-01 00:20:18 -0800

    Can now build with c++17 and lakelib linked

--- test/scripts/mac_settings.sh
+++ test/scripts/mac_settings.sh
@@ -1,2 +1,3 @@
 export LLVM_CONFIG=/usr/local/Cellar/llvm@6/6.0.1_1/bin/llvm-config
 export CLANG=/usr/local/Cellar/llvm@6/6.0.1_1/bin/clang
+export CXX=/Users/dillon/Downloads/clang+llvm-9.0.0-x86_64-darwin-apple/bin/clang++
