87d758323ec2e1c96d8891cb232299fc3320b505
Jeff Setter <setter@stanford.edu>
2020-03-17 15:58:21 -0700

    Create coreir before buffermapping

--- test/scripts/garnet_docker_build.sh
+++ test/scripts/garnet_docker_build.sh
@@ -17,9 +17,14 @@ docker exec -i halide-hw-distro bash -c "ls /GarnetFlow/scripts"
 docker exec -i halide-hw-distro bash -c "ls /GarnetFlow/scripts/Halide-to-Hardware/"
 
 echo 'Running Halide build...'
+# Build coreir
+docker exec -i halide-hw-distro bash -c "cd /GarnetFlow/scripts; 	git clone --depth 1 --branch rm-ubuf-master https://github.com/rdaly525/coreir coreir"
+docker exec -i halide-hw-distro bash -c "cd /GarnetFlow/scripts/coreir/build && cmake .. && make -j4"
+# Build BufferMapping
 docker exec -i halide-hw-distro bash -c "cd /GarnetFlow/scripts; git clone --branch accumulation --depth 1 https://github.com/joyliu37/BufferMapping"
 docker exec -i halide-hw-distro bash -c "cd /GarnetFlow/scripts/BufferMapping/cfunc; make lib"
 docker exec -i halide-hw-distro bash -c "ls /GarnetFlow/scripts/BufferMapping"
+# Run rest of GarnetFlow build script
 docker exec -i halide-hw-distro bash -c "cd /GarnetFlow/scripts; ./build_halide_distrib.sh"
 
 # Run the testbench in docker, copy the result to the local travis machine, and
