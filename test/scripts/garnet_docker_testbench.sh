dab69d42697b6c4c8b501c161ce7ed327befd86d
joyliu37 <joyliu37@sina.com>
2020-03-23 14:13:20 -0700

    comment the garnet test

--- test/scripts/garnet_docker_testbench.sh
+++ test/scripts/garnet_docker_testbench.sh
@@ -15,32 +15,32 @@ else
   echo 'HW Unit tests PASSED'
 fi
 
-echo 'Running pointwise...'
-./test/scripts/compare_app.sh pointwise >& pw_res.txt
-pw_res=$?
-echo 'Pointwise result:'
-echo $pw_res
-
-if [ $pw_res != "0" ]; then
-  echo 'Pointwise FAILED'
-  echo 'FAILED' > tb_res.txt
-  exit 1
-else
-  echo 'pointwise PASSED'
-fi
-
-echo 'Running conv_3_3...'
-./test/scripts/compare_test.sh conv_3_3 >& conv_res.txt
-pw_res=$?
-echo 'conv_3_3 result:'
-echo $pw_res
-
-if [ $pw_res != "0" ]; then
-  echo 'conv_3_3 FAILED'
-  echo 'FAILED' > tb_res.txt
-  exit 1
-else
-  echo 'conv_3_3 PASSED'
-fi
-
-echo 'passed' > tb_res.txt
+#echo 'Running pointwise...'
+#./test/scripts/compare_app.sh pointwise >& pw_res.txt
+#pw_res=$?
+#echo 'Pointwise result:'
+#echo $pw_res
+#
+#if [ $pw_res != "0" ]; then
+#  echo 'Pointwise FAILED'
+#  echo 'FAILED' > tb_res.txt
+#  exit 1
+#else
+#  echo 'pointwise PASSED'
+#fi
+#
+#echo 'Running conv_3_3...'
+#./test/scripts/compare_test.sh conv_3_3 >& conv_res.txt
+#pw_res=$?
+#echo 'conv_3_3 result:'
+#echo $pw_res
+#
+#if [ $pw_res != "0" ]; then
+#  echo 'conv_3_3 FAILED'
+#  echo 'FAILED' > tb_res.txt
+#  exit 1
+#else
+#  echo 'conv_3_3 PASSED'
+#fi
+#
+#echo 'passed' > tb_res.txt
