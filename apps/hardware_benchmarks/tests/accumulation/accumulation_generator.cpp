f8ff0e1fb48f32c711a955047e97d4e9096bd4e4
joyliu37 <joyliu37@sina.com>
2020-04-21 11:12:14 -0700

    add rewrite test

--- apps/hardware_benchmarks/tests/accumulation/accumulation_generator.cpp
+++ apps/hardware_benchmarks/tests/accumulation/accumulation_generator.cpp
@@ -7,7 +7,7 @@ using namespace Halide;
 float gamma = 1.0f;
 float contrast = 1.0f;
 
-class MemTestAccumulation : public Halide::Generator<UnitTestAccumulation> {
+class MemTestAccumulation : public Halide::Generator<MemTestAccumulation> {
 public:
     Input<Buffer<uint8_t>>  input{"input", 2};
     Output<Buffer<uint8_t>> output{"output", 2};
@@ -32,7 +32,7 @@ public:
                             a*g*g + b*g);
           curve(x) = cast<uint8_t>(clamp(val*256.0f, 0.0f, 255.0f));
         }
-        
+
         Expr in_val = clamp(hw_input(x, y), 0, 1023);
         lookup(x,y) = curve(in_val);
 
@@ -48,7 +48,7 @@ public:
           output.bound(y, 0, 64);
 
           hw_output.compute_root();
-          
+
           hw_output.tile(x,y, xo,yo, xi,yi, 64, 64)
             .hw_accelerate(xi, xo);
 
@@ -56,11 +56,11 @@ public:
 
           hw_input.compute_at(hw_output, xi).store_at(hw_output, xo);
           hw_input.stream_to_accelerator();
-          
+
         } else {  // schedule to CPU
           output.compute_root();
         }
-        
+
     }
 };
 
--- apps/hardware_benchmarks/tests/accumulation/process.cpp
+++ apps/hardware_benchmarks/tests/accumulation/process.cpp
@@ -1,6 +1,6 @@
 #include <cstdio>
 
-#include "rom.h"
+#include "accumulation.h"
 
 #include "hardware_process_helper.h"
 #include "coreir_interpret.h"
@@ -11,10 +11,14 @@ using namespace Halide::Runtime;
 
 int main(int argc, char **argv) {
 
-  OneInOneOut_ProcessController<uint8_t> processor("rom",
+  OneInOneOut_ProcessController<uint8_t> processor("accumulation",
                                           {
                                             {"cpu",
-                                                [&]() { rom(processor.input, processor.output); }
+                                                [&]() { accumulation(processor.input, processor.output); }
+                                            },
+                                            {"rewrite",
+                                                [&]() { run_coreir_rewrite_on_interpreter<>("bin/design_top.json", "bin/ubuffers.json", processor.input, processor.output,
+                                                                                    "self.in_arg_0_0_0", "self.out_0_0"); }
                                             },
                                             {"coreir",
                                                 [&]() { run_coreir_on_interpreter<>("bin/design_top.json", processor.input, processor.output,
@@ -24,7 +28,7 @@ int main(int argc, char **argv) {
 
   processor.input = Buffer<uint8_t>(64, 64);
   processor.output = Buffer<uint8_t>(64, 64);
-  
+
   processor.process_command(argc, argv);
-  
+
 }
