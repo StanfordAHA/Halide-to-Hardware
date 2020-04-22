28183935a7707e43b1156469b097379e20b0c3c1
Jeff Setter <setter@stanford.edu>
2020-04-22 15:06:21 -0700

    Fix llvm installation

--- test/scripts/garnet_docker_build.sh
+++ test/scripts/garnet_docker_build.sh
@@ -25,8 +25,8 @@ docker exec -i halide-hw-distro bash -c "cd /GarnetFlow/scripts; git clone --bra
 docker exec -i halide-hw-distro bash -c "cd /GarnetFlow/scripts/BufferMapping/cfunc; make lib"
 docker exec -i halide-hw-distro bash -c "ls /GarnetFlow/scripts/BufferMapping"
 # Build LLVM
-docker exec -i halide-hw-distro bash -c "export LLVM_VERSION=7"
-docker exec -i halide-hw-distro bash -c "cd /GarnetFlow/scripts/Halide-to-Hardware; ./test/scripts/install_travis.sh; cp -r ${TRAVIS_BUILD_DIR}/llvm /usr/lib/llvm-7"
+docker exec -i halide-hw-distro bash -c "export LLVM_VERSION=7; cd /GarnetFlow/scripts/Halide-to-Hardware; ./test/scripts/install_travis.sh; cp -r ${TRAVIS_BUILD_DIR}/llvm /usr/lib/llvm-7"
+docker exec -i halide-hw-distro bash -c "apt-get update"
 # Run rest of GarnetFlow build script
 docker exec -i halide-hw-distro bash -c "cd /GarnetFlow/scripts; ./build_halide_distrib.sh"
 
