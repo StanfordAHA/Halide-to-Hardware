e0818863e383ba90e36d97ebf6d927362d603628
Jeff Setter <setter@stanford.edu>
2020-07-30 16:51:15 -0700

    Mobilenet now working

--- apps/hardware_benchmarks/apps/conv_layer_mobile/conv_layer_generator.cpp
+++ apps/hardware_benchmarks/apps/conv_layer_mobile/conv_layer_generator.cpp
@@ -3,14 +3,17 @@
 namespace {
 
 using namespace Halide;
+using namespace Halide::ConciseCasts;
 
 class ConvolutionLayer : public Halide::Generator<ConvolutionLayer> {
 public:
-    Input<Buffer<int8_t>>  input{"input", 3};
+    Input<Buffer<uint8_t>>  input{"input", 3};
     Output<Buffer<uint8_t>> output{"output", 3};
 
   void generate() {
         /* THE ALGORITHM */
+        int num_in_ch = 4;
+        int num_out_ch = 3;
 
         Func input_copy("input_copy");
         Func hw_input("hw_input");
@@ -20,10 +23,10 @@ public:
         Func filter_dw, filter_pw;
         Func bias_dw, bias_pw;
 
-        filter_dw(x, y, c) = 1;
-        filter_pw(c, k) = 3;
-        bias_dw(c) = 0;
-        bias_pw(k) = 0;
+        filter_dw(x, y, c) = u16(1);
+        filter_pw(c, k) = u16(3);
+        bias_dw(c) = u16(0);
+        bias_pw(k) = u16(0);
 
         Func dw_conv("dw_conv");
         Func pw_conv("pw_conv");
@@ -31,25 +34,26 @@ public:
 
         RDom r_dw(0, 3, 0, 3);
         RVar pw_c("c");
-        RDom r_pw(0, 4);
+        RDom r_pw(0, num_in_ch);
 
-        input_copy(x, y, c) = input(x, y, c);
+        input_copy(x, y, c) = u16(input(x, y, c));
         hw_input(x, y, c) = input_copy(x, y, c);
 
         //depthwise ConvolutionLayer
-        dw_conv(x, y, c) = 0;//cast<int16_t>(bias_dw(c));
-        dw_conv(x, y, c) += cast<int16_t>(filter_dw(r_dw.x, r_dw.y, c) *
-                                          hw_input(x + r_dw.x, y + r_dw.y, c));
+        dw_conv(x, y, c) = i16(0);//cast<int16_t>(bias_dw(c));
+        dw_conv(x, y, c) += i16(filter_dw(r_dw.x, r_dw.y, c) *
+                                hw_input(x + r_dw.x, y + r_dw.y, c));
 
         //pointwise ConvolutionLayer
-        pw_conv(k, x, y, c) = 0;//cast<int16_t>(bias_pw(k));
-        pw_conv(k, x, y, c) += cast<int16_t>(filter_pw(k, c) * dw_conv(x, y, c));
-        pw_conv_reduction(k, x, y) = 0;
-        pw_conv_reduction(k, x, y) += cast<int16_t>(pw_conv(k, x, y, r_pw.x));
-        hw_output(k, x, y) = cast<int8_t>(max(0, pw_conv_reduction(k, x, y)));
+        pw_conv(k, x, y, c) = i16(0);//cast<int16_t>(bias_pw(k));
+        pw_conv(k, x, y, c) += i16(filter_pw(k, c) * dw_conv(x, y, c));
+        pw_conv_reduction(k, x, y) = i16(0);
+        pw_conv_reduction(k, x, y) += i16(pw_conv(k, x, y, r_pw.x));
+        hw_output(x, y, k) = u8(max(0, pw_conv_reduction(k, x, y)));
+        //hw_output(x, y, k) = u8(hw_input(x, y, 0));
         //hw_output(x, y, k) = cast<int8_t>(max(0, dw_conv(x, y, k)));
 
-        output(k, x, y) = cast<uint8_t>(hw_output(k, x, y));
+        output(x, y, k) = hw_output(x, y, k);
 
         /* THE SCHEDULE */
         if (get_target().has_feature(Target::CoreIR)) {
@@ -122,7 +126,8 @@ public:
           dw_conv.update()
             .reorder(r_dw.x, r_dw.y, x, y, c)
             .unroll(r_dw.x)
-            .unroll(r_dw.y);
+            .unroll(r_dw.y)
+            .unroll(c);
           //.unroll(c);
           
 
@@ -135,28 +140,29 @@ public:
         } else if (get_target().has_feature(Target::Clockwork)) {
           output.bound(x, 0, 16);
           output.bound(y, 0, 16);
-          output.bound(k, 0, 4);
+          output.bound(k, 0, num_out_ch); //output channel
 
           pw_conv_reduction.bound(x, 0, 16);
           pw_conv_reduction.bound(y, 0, 16);
-          pw_conv_reduction.bound(k, 0, 4);
+          pw_conv.bound(c, 0, num_in_ch);
 
           // Blocking spatially on X Y dim
           Var xo("xo"), xi("xi"), yo("yo"), yi("yi");
           
           hw_output.compute_root();
 
-          hw_output.tile(x, y, xo, yo, xi, yi, 14, 14)
-            .reorder(k, xi, yi, xo, yo)
-            .reorder_storage(k, x, y);
+          hw_output.tile(x, y, xo, yo, xi, yi, 16, 16)
+            .reorder(xi, yi, k, xo, yo)
+            //.reorder_storage(k, x, y)
+            .hw_accelerate(xi, xo);
 
-          filter_dw.compute_at(hw_output, yo)
+          filter_dw.compute_at(hw_output, xo)
             .unroll(x).unroll(y).unroll(c);
-          filter_pw.compute_at(hw_output, yo)
+          filter_pw.compute_at(hw_output, xo)
             .unroll(c).unroll(k);
-          bias_dw.compute_at(hw_output, yo)
+          bias_dw.compute_at(hw_output, xo)
             .unroll(c);
-          bias_pw.compute_at(hw_output, yo)
+          bias_pw.compute_at(hw_output, xo)
             .unroll(k);
 
           //schedule pw conv reduction
@@ -169,18 +175,25 @@ public:
           //schedule pw conv
           pw_conv.compute_at(hw_output, xo).store_at(hw_output, xo)
             .reorder(c, x, y, k);
+
+          pw_conv.update()
+            .unroll(c)
+            .unroll(k);
           
           //schedule dw conv
-          dw_conv.compute_at(pw_conv, x).store_at(hw_output, xo)
+          //dw_conv.compute_at(pw_conv, x).store_at(hw_output, xo)
+          dw_conv.compute_at(hw_output, xo)
             .reorder(x, y, c);
           
           dw_conv.update()
             .reorder(r_dw.x, r_dw.y, x, y, c)
             .unroll(r_dw.x)
-            .unroll(r_dw.y);
+            .unroll(r_dw.y)
+            .unroll(c);
 
           //add input stream
           hw_input.store_at(hw_output, xo).compute_at(hw_output, xo);
+          input_copy.stream_to_accelerator();
           input_copy.compute_root();
         }
 
--- apps/hardware_benchmarks/apps/conv_layer_mobile/process.cpp
+++ apps/hardware_benchmarks/apps/conv_layer_mobile/process.cpp
@@ -1,4 +1,120 @@
 #include <cstdio>
+#include "hardware_process_helper.h"
+#include "halide_image_io.h"
+
+#if defined(WITH_CPU)
+   #include "conv_layer.h"
+#endif
+
+#if defined(WITH_COREIR)
+    #include "coreir_interpret.h"
+#endif
+
+#if defined(WITH_CLOCKWORK)
+    #include "rdai_api.h"
+    #include "clockwork_sim_platform.h"
+    #include "conv_layer_clockwork.h"
+#endif
+
+using namespace Halide::Tools;
+using namespace Halide::Runtime;
+
+int main( int argc, char **argv ) {
+  std::map<std::string, std::function<void()>> functions;
+  OneInOneOut_ProcessController<uint8_t> processor("conv_layer");
+
+  #if defined(WITH_CPU)
+      auto cpu_process = [&]( auto &proc ) {
+        conv_layer( proc.input, proc.output );
+      };
+      functions["cpu"] = [&](){ cpu_process( processor ); } ;
+  #endif
+  
+  #if defined(WITH_COREIR)
+      auto coreir_process = [&]( auto &proc ) {
+          run_coreir_on_interpreter<>( "bin/design_top.json",
+                                       proc.input, proc.output,
+                                       "self.in_arg_0_0_0", "self.out_0_0" );
+      };
+      functions["coreir"] = [&](){ coreir_process( processor ); };
+  #endif
+  
+  #if defined(WITH_CLOCKWORK)
+      auto clockwork_process = [&]( auto &proc ) {
+        RDAI_Platform *rdai_platform = RDAI_register_platform( &rdai_clockwork_sim_ops );
+        if ( rdai_platform ) {
+          printf( "[RUN_INFO] found an RDAI platform\n" );
+          conv_layer_clockwork( proc.input, proc.output );
+          RDAI_unregister_platform( rdai_platform );
+        } else {
+          printf("[RUN_INFO] failed to register RDAI platform!\n");
+        }
+      };
+      functions["clockwork"] = [&](){ clockwork_process( processor ); };
+  #endif
+
+  // Add all defined functions
+  processor.run_calls = functions;
+
+    //int Batch = 1;
+    int Height = 16;//64;
+    int Width = 16;//64;
+    int Chin = 4;
+    int Chout = 3; //32;
+    int Filter_sz = 3;
+
+    //Buffer<int8_t> input(Width + Filter_sz - 1, Height + Filter_sz - 1, Chin, Batch);
+    Buffer<uint8_t> input(Width + Filter_sz - 1, Height + Filter_sz - 1, Chin);
+    Buffer<int8_t> filter_dw(Filter_sz, Filter_sz, Chin);
+    Buffer<int8_t> filter_pw(Chin, Chout);
+    Buffer<int8_t> bias_dw(Chin);
+    Buffer<int8_t> bias_pw(Chout);
+
+    //for (int n = 0; n < input.dim(3).extent(); n++)
+    for (int c = 0; c < input.channels(); c++)
+    for (int h = 0; h < input.height(); h++)
+    for (int w = 0; w < input.width(); w++) {
+      //input(w, h, c, n) = (int8_t)rand();
+      //input(w, h, c) = (int8_t)rand();
+      input(w, h, c) = w + h + c;
+    }
+
+    for (int c = 0; c < filter_dw.channels(); c++)
+    for (int y = 0; y < filter_dw.height(); y++)
+    for (int x = 0; x < filter_dw.width(); x++) {
+          filter_dw(x, y, c) = (int8_t)rand();
+    }
+
+    for (int k = 0; k < filter_pw.height(); k++)
+    for (int c = 0; c < filter_pw.width(); c++) {
+          filter_pw(c, k) = (int8_t)rand();
+    }
+
+    for (int x = 0; x < bias_dw.width(); x++) {
+          bias_dw(x) = (int8_t)rand();
+    }
+
+    for (int x = 0; x < bias_pw.width(); x++) {
+          bias_pw(x) = (int8_t)rand();
+    }
+
+    //Buffer<uint8_t> output(Width, Height, Chout, Batch);
+    Buffer<uint8_t> output(Width, Height, Chout);
+
+    //conv_layer(input, filter_dw, filter_pw, bias_dw, bias_pw, output);
+
+  processor.input   = input;
+  processor.output  = output;
+  processor.inputs_preset = true;
+  
+  processor.process_command(argc, argv);
+  
+}
+
+
+/*
+
+#include <cstdio>
 #include <chrono>
 
 #include "conv_layer.h"
@@ -77,3 +193,4 @@ int main(int argc, char **argv) {
 
     return 0;
 }
+*/
