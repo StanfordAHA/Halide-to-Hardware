42691a80dec54cabeace57ffca6a9c5808437dcb
Charles Tsao <chtsao@stanford.edu>
2020-08-03 12:46:15 -1000

    updated equal and conv_rolled tests; bitwidth renamed to casts

--- apps/hardware_benchmarks/tests/conv_rolled/conv_rolled_generator.cpp
+++ apps/hardware_benchmarks/tests/conv_rolled/conv_rolled_generator.cpp
@@ -3,6 +3,7 @@
 namespace {
 
 using namespace Halide;
+using namespace Halide::ConciseCasts;
 
 class ConvolutionKernel : public Halide::Generator<ConvolutionKernel> {
 public:
@@ -10,7 +11,7 @@ public:
     Output<Buffer<uint8_t>> output{"output", 2};
 
   int ksize = 3;
-  int imgsize = 12;
+  int imgsize = 62;
 
     void generate() {
         /* THE ALGORITHM */
@@ -29,8 +30,10 @@ public:
         conv(x, y) = 0;
 
         Func hw_input("hw_input");
+        Func hw_input_copy("hw_input_copy");
         hw_input(x, y) = cast<uint16_t>(input(x, y));
-        conv(x, y)  += kernel(r.x, r.y) * hw_input(x + r.x, y + r.y);
+        hw_input_copy(x, y) = hw_input(x, y);
+        conv(x, y)  += cast<uint16_t>(kernel(r.x, r.y)) * hw_input_copy(x + r.x, y + r.y);
 
         Func hw_output("hw_output");
         hw_output(x, y) = cast<uint8_t>(conv(x, y));
@@ -70,24 +73,24 @@ public:
           kernel.compute_at(hw_output, yi);
           //kernel.compute_root();
 
-        } else {  // schedule to CPU
-//          kernel.compute_root();
-//          conv.compute_root();
-//          conv.update()
-//            .unroll(r.x, ksize)
-//            .unroll(r.y, ksize);
+        } else if (get_target().has_feature(Target::Clockwork)) {
+          Var xi,yi, xo,yo;
 
-//          Var xi,yi, xo,yo;
-//          output.tile(x, y, xo,yo, xi,yi, 64,64);
-//          hw_input.in().store_at(output, xo).compute_at(output, xi);
-//          //hw_input.in().store_root().compute_at(output, x);
-          //hw_input.in().store_root().compute_at(output,x);
-          conv.update()
-            .unroll(r.x)
-            .unroll(r.y);
-//          //output.compute_root();
-//
-//          kernel.compute_at(output, xo);
+          hw_output.compute_root();
+
+          hw_output
+              .tile(x,y, xo,yo, xi,yi, imgsize, imgsize)
+              .hw_accelerate(xi, xo);
+
+          conv.compute_at(hw_output, xo);
+          kernel.compute_at(conv, x);
+
+          hw_input_copy.compute_at(hw_output, xo);
+          hw_input.compute_root();
+          hw_input.stream_to_accelerator();
+            
+        } else {  // schedule to CPU
+            output.compute_root();
         }
 
     }
--- apps/hardware_benchmarks/tests/equal/equal_generator.cpp
+++ apps/hardware_benchmarks/tests/equal/equal_generator.cpp
@@ -15,7 +15,9 @@ public:
         Var x("x"), y("y");
 
         Func hw_input("hw_input");
-        hw_input(x, y) = cast<int16_t>(input(x, y));
+        Func hw_input_copy("hw_input_copy");
+        hw_input(x, y) = cast<uint16_t>(input(x, y));
+        hw_input_copy(x, y) = hw_input(x, y);
 
         Func eq, ne;
         eq(x,y) = hw_input(x,y) == 128;
@@ -41,6 +43,24 @@ public:
           hw_input.compute_at(hw_output, xi).store_at(hw_output, xo);
           hw_input.stream_to_accelerator();
           
+        } else if (get_target().has_feature(Target::Clockwork)) {
+          Var xi,yi, xo,yo;
+
+          output.bound(x, 0, 64);
+          output.bound(y, 0, 64);
+
+          hw_output.compute_root();
+
+          hw_output
+              .tile(x,y, xo,yo, xi,yi, 64, 64)
+              .hw_accelerate(xi, xo);
+
+          //conv.compute_at(hw_output, xo);
+
+          hw_input_copy.compute_at(hw_output, xo);
+          hw_input.compute_root();
+          hw_input.stream_to_accelerator();
+            
         } else {  // schedule to CPU
           output.compute_root();
         }
