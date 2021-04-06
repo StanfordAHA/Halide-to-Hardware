d3bbe7315fe3579e411672ae0bc56c28d467bee0
joyliu37 <joyliu37@sina.com>
2021-04-06 13:48:28 -0700

    add conv_3_3_chain

---
+++ apps/hardware_benchmarks/tests/conv_3_3_chain/conv_3_3_generator.cpp
@@ -0,0 +1,88 @@
+#include "Halide.h"
+
+namespace {
+
+using namespace Halide;
+
+class ConvolutionKernel : public Halide::Generator<ConvolutionKernel> {
+public:
+    Input<Buffer<uint8_t>>  input{"input", 2};
+    Output<Buffer<uint8_t>> output{"output", 2};
+
+  int ksize = 3;
+  int imgsize = 62;
+
+    void generate() {
+        /* THE ALGORITHM */
+
+        Var x("x"), y("y");
+
+        Func kernel("kernel");
+        Func conv("conv");
+        RDom r(0, ksize,               0, ksize);
+
+        kernel(x,y) = 0;
+        kernel(0,0) = 11;      kernel(0,1) = 12;      kernel(0,2) = 13;
+        kernel(1,0) = 14;      kernel(1,1) = 0;       kernel(1,2) = 16;
+        kernel(2,0) = 17;      kernel(2,1) = 18;      kernel(2,2) = 19;
+
+        conv(x, y) = 0;
+
+        Func hw_input("hw_input");
+        hw_input(x, y) = cast<uint16_t>(input(x, y));
+        conv(x, y)  += kernel(r.x, r.y) * hw_input(x + r.x, y + r.y);
+
+        Func hw_output("hw_output");
+        hw_output(x, y) = cast<uint8_t>(conv(x, y));
+        output(x, y) = hw_output(x,y);
+
+        output.bound(x, 0, imgsize);
+        output.bound(y, 0, imgsize);
+
+        /* THE SCHEDULE */
+        if (get_target().has_feature(Target::CoreIR)) {
+          Var xi,yi, xo,yo;
+
+          hw_input.compute_root();
+          hw_output.compute_root();
+
+          hw_output.tile(x,y, xo,yo, xi,yi, imgsize, imgsize)
+            .hw_accelerate(xi, xo);
+
+          conv.update()
+            .unroll(r.x, ksize)
+            .unroll(r.y, ksize);
+
+          conv.linebuffer();
+
+          //hw_input.linebuffer();
+
+          hw_input.stream_to_accelerator();
+          kernel.compute_at(hw_output, xo).unroll(x).unroll(y);
+
+        } else {  // schedule to CPU
+//          kernel.compute_root();
+//          conv.compute_root();
+//          conv.update()
+//            .unroll(r.x, ksize)
+//            .unroll(r.y, ksize);
+
+//          Var xi,yi, xo,yo;
+//          output.tile(x, y, xo,yo, xi,yi, 64,64);
+//          hw_input.in().store_at(output, xo).compute_at(output, xi);
+//          //hw_input.in().store_root().compute_at(output, x);
+          //hw_input.in().store_root().compute_at(output,x);
+          conv.update()
+            .unroll(r.x)
+            .unroll(r.y);
+//          //output.compute_root();
+//
+//          kernel.compute_at(output, xo);
+        }
+
+    }
+};
+
+}  // namespace
+
+HALIDE_REGISTER_GENERATOR(ConvolutionKernel, conv_3_3)
