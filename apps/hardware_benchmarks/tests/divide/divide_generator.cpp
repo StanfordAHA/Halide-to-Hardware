fcdd8733a8093149cf4c376053be66ccc4243603
Jeff Setter <setter@stanford.edu>
2021-04-08 10:57:51 -0700

    Modify arith test to remove complicated srem; move to new divide test

--- apps/hardware_benchmarks/tests/divide/divide_generator.cpp
+++ apps/hardware_benchmarks/tests/divide/divide_generator.cpp
@@ -5,7 +5,7 @@ namespace {
 using namespace Halide;
 using namespace Halide::ConciseCasts;
 
-class UnitTestArith : public Halide::Generator<UnitTestArith> {
+class UnitTestDiv : public Halide::Generator<UnitTestDiv> {
 public:
     Input<Buffer<uint8_t>>  input{"input", 2};
     Output<Buffer<uint8_t>> output{"output", 2};
@@ -18,31 +18,20 @@ public:
         Func hw_input("hw_input");
         hw_input(x, y) = u16(input(x, y));
 
-        Func mult, div, add, sub, mod;
-        mult(x,y) = hw_input(x,y) * 13;
-        div(x,y) = hw_input(x,y) / 4;
-        mod(x,y) = hw_input(x,y) % 16;
-        add(x,y) = div(x,y) + mod(x,y);
-        sub(x,y) = mult(x,y) - add(x,y);
+        Func udiv, sdiv, urem, srem;
+        udiv(x,y) = hw_input(x,y) / 5;
+        urem(x,y) = hw_input(x,y) % 7;
+        sdiv(x,y) = i16(udiv(x,y) - urem(x,y)) / -100;
+        //srem(x,y) = sdiv(x,y) % -3;
+        srem(x,y) = -3 % min(sdiv(x,y), -1);
+        // could also test with data in the denominator
 
         Func hw_output("hw_output");
-        hw_output(x, y) = sub(x, y);
+        hw_output(x, y) = srem(x, y);
         output(x, y) = u8(hw_output(x,y));
         
         /* THE SCHEDULE */
         if (get_target().has_feature(Target::CoreIR)) {
-          Var xi,yi, xo,yo;
-          
-          hw_input.compute_root();
-          hw_output.compute_root();
-
-          output.bound(x, 0, 64);
-          output.bound(y, 0, 64);
-          
-          hw_output.tile(x,y, xo,yo, xi,yi, 64, 64)
-            .hw_accelerate(xi, xo);
-
-          hw_input.stream_to_accelerator();
           
         } else if (get_target().has_feature(Target::Clockwork)) {
           Var xi,yi, xo,yo;
@@ -66,4 +55,4 @@ public:
 
 }  // namespace
 
-HALIDE_REGISTER_GENERATOR(UnitTestArith, arith)
+HALIDE_REGISTER_GENERATOR(UnitTestDiv, divide)
---
+++ apps/hardware_benchmarks/tests/divide/process.cpp
@@ -0,0 +1,80 @@
+#include <cstdio>
+#include "hardware_process_helper.h"
+#include "halide_image_io.h"
+
+#if defined(WITH_CPU)
+   #include "divide.h"
+#endif
+
+#if defined(WITH_COREIR)
+    #include "coreir_interpret.h"
+#endif
+
+#if defined(WITH_CLOCKWORK)
+    #include "rdai_api.h"
+    #include "clockwork_sim_platform.h"
+    #include "divide_clockwork.h"
+#endif
+
+using namespace Halide::Tools;
+using namespace Halide::Runtime;
+
+int main( int argc, char **argv ) {
+  std::map<std::string, std::function<void()>> functions;
+  OneInOneOut_ProcessController<uint8_t> processor("divide");
+
+  #if defined(WITH_CPU)
+      auto cpu_process = [&]( auto &proc ) {
+        divide( proc.input, proc.output );
+      };
+      functions["cpu"] = [&](){ cpu_process( processor ); } ;
+  #endif
+  
+  #if defined(WITH_COREIR)
+      auto coreir_process = [&]( auto &proc ) {
+          run_coreir_on_interpreter<>( "bin/design_top.json",
+                                       proc.input, proc.output,
+                                       "self.in_arg_0_0_0", "self.out_0_0" );
+      };
+      functions["coreir"] = [&](){ coreir_process( processor ); };
+  #endif
+  
+  #if defined(WITH_CLOCKWORK)
+      auto clockwork_process = [&]( auto &proc ) {
+        RDAI_Platform *rdai_platform = RDAI_register_platform( &rdai_clockwork_sim_ops );
+        if ( rdai_platform ) {
+          printf( "[RUN_INFO] found an RDAI platform\n" );
+          divide_clockwork( proc.input, proc.output );
+          RDAI_unregister_platform( rdai_platform );
+        } else {
+          printf("[RUN_INFO] failed to register RDAI platform!\n");
+        }
+      };
+      functions["clockwork"] = [&](){ clockwork_process( processor ); };
+  #endif
+
+  // Add all defined functions
+  processor.run_calls = functions;
+  //processor.inputs_preset = true;
+
+  processor.input   = Buffer<uint8_t>(64, 64);
+  processor.output  = Buffer<uint8_t>(64, 64);
+
+  processor.input(0, 0) = -2;
+  processor.input(0, 1) = -1;
+  processor.input(0, 2) = 0;
+  processor.input(0, 3) = 1;
+  processor.input(0, 4) = 2;
+  
+  
+  auto return_value = processor.process_command(argc, argv);
+
+  std::cout << "out(0,0) = " << +processor.output(0, 0) << std::endl;
+  std::cout << "out(0,1) = " << +processor.output(0, 1) << std::endl;
+  std::cout << "out(0,2) = " << +processor.output(0, 2) << std::endl;
+  std::cout << "out(0,3) = " << +processor.output(0, 3) << std::endl;
+  std::cout << "out(0,4) = " << +processor.output(0, 4) << std::endl;
+
+  return return_value;
+  
+}
