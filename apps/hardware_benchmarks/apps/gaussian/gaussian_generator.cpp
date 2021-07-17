86c518ef10c81e1090f6e8729e42f439b1f9a79b
Jeff Setter <setter@stanford.edu>
2021-07-16 18:37:58 -0700

    Add Gaussian schedules with glb and tiling; Add glb channel reduction test

--- apps/hardware_benchmarks/apps/gaussian/gaussian_generator.cpp
+++ apps/hardware_benchmarks/apps/gaussian/gaussian_generator.cpp
@@ -11,18 +11,16 @@ using namespace Halide;
 
 // Size of blur for gradients.
 const int blockSize = 3;
-//const int inputSize = 1242;
-const int inputSize = 64;
-const int imgSize = inputSize-blockSize+1;
 
 class GaussianBlur : public Halide::Generator<GaussianBlur> {
 public:
     Input<Buffer<uint8_t>>  input{"input", 2};
     Output<Buffer<uint8_t>> output{"output", 2};
+  
+    GeneratorParam<uint8_t> schedule{"schedule", 0};    // default: 0
 
   //Input<int32_t> tilesize{"tilesize", 64, 8, 128}; // default 64. bounded between 8 and 128
   //int tilesize = imgSize / 2;
-  int tilesize = 62;
 
     void generate() {
         /* THE ALGORITHM */
@@ -76,63 +74,105 @@ public:
         /* THE SCHEDULE */
         if (get_target().has_feature(Target::CoreIR)) {
 
-          hw_output.bound(x, 0, imgSize);
-          hw_output.bound(y, 0, imgSize);
-          output.bound(x, 0, imgSize);
-          output.bound(y, 0, imgSize);
-          blur_unnormalized.bound(x, 0, imgSize);
-          blur_unnormalized.bound(y, 0, imgSize);
-
-          //hw_input.compute_root();
-          //kernel.compute_root();
-          hw_output.compute_root();
-
-          hw_output
-            //            .compute_at(output, xo)
-            .tile(x, y, xo, yo, xi, yi, imgSize, imgSize)
-            .hw_accelerate(xi, xo);
-
-          blur_unnormalized.update()
-            .unroll(win.x, blockSize)
-            .unroll(win.y, blockSize);
-
-          blur_unnormalized.linebuffer();
-
-          //hw_output.accelerate({hw_input}, xi, xo);
-          hw_input.compute_at(hw_output, xi).store_at(hw_output, xo);
-          hw_input.stream_to_accelerator();
 
         } else if (get_target().has_feature(Target::Clockwork)) {
-          output.bound(x, 0, imgSize);
-          output.bound(y, 0, imgSize);
 
-          //output.bound(x, 0, tilesize);
-          //output.bound(y, 0, tilesize);
+          if (schedule == 1) {
+            // use global buffer and large input image
+            const int tileSize = 62;
+            const int numTiles = 4;
+            const int glbSize = tileSize * numTiles;
+            const int numHostTiles = 5;
+            const int outputSize = numHostTiles * glbSize;
+            const int inputSize = outputSize + blockSize-1;
+            
+            output.bound(x, 0, outputSize);
+            output.bound(y, 0, outputSize);
 
-          hw_output.compute_root();
+            hw_output.in().compute_root();
 
-          hw_output
-            //.tile(x, y, xo, yo, xi, yi, imgSize, imgSize)
-            .tile(x, y, xo, yo, xi, yi, tilesize, tilesize)
-            .hw_accelerate(xi, xo);
+            hw_output.in()
+              .tile(x, y, xo, yo, xi, yi, glbSize, glbSize)
+              .hw_accelerate(xi, xo);
 
-          blur_unnormalized.update()
-            .unroll(win.x, blockSize)
-            .unroll(win.y, blockSize);
+            Var xii, yii, xio, yio;
+            hw_output
+              .tile(x, y, xo, yo, xi, yi, tileSize, tileSize);
+            hw_output.compute_at(hw_output.in(), xo);
 
-          blur_unnormalized.compute_at(hw_output, xo);
-          blur.compute_at(hw_output, xo);
+            blur_unnormalized.update()
+              .unroll(win.x, blockSize)
+              .unroll(win.y, blockSize);
 
-          hw_input.stream_to_accelerator();//.compute_root();//at(output, xo);
-          //hw_input.compute_root();
+            blur_unnormalized.compute_at(hw_output, xo);
+            blur.compute_at(hw_output, xo);
+
+            hw_input.in().in().compute_at(hw_output, xo);
+            hw_input.in().compute_at(hw_output.in(), xo);
+            hw_input.compute_root()
+              .accelerator_input();
+
+          } else if (schedule == 2) {
+            // Perform host tiliing
+            const int inputSize = 64;
+            const int outputSize = inputSize-blockSize+1;
+            const int tileSize = outputSize / 2; // four small tiles
+            
+            output.bound(x, 0, outputSize);
+            output.bound(y, 0, outputSize);
+
+            hw_output.compute_root();
+
+            hw_output
+              .tile(x, y, xo, yo, xi, yi, tileSize, tileSize)
+              .hw_accelerate(xi, xo);
+
+            blur_unnormalized.update()
+              .unroll(win.x, blockSize)
+              .unroll(win.y, blockSize);
+
+            blur_unnormalized.compute_at(hw_output, xo);
+            blur.compute_at(hw_output, xo);
+            
+            hw_input.stream_to_accelerator();
+            
+          } else {
+            const int inputSize = 64;
+            const int outputSize = inputSize-blockSize+1;
+            const int tileSize = outputSize; // single tile
+            
+            output.bound(x, 0, outputSize);
+            output.bound(y, 0, outputSize);
+
+            //output.bound(x, 0, tileSize);
+            //output.bound(y, 0, tileSize);
+
+            hw_output.compute_root();
+
+            hw_output
+              //.tile(x, y, xo, yo, xi, yi, outputSize, outputSize)
+              .tile(x, y, xo, yo, xi, yi, tileSize, tileSize)
+              .hw_accelerate(xi, xo);
+
+            blur_unnormalized.update()
+              .unroll(win.x, blockSize)
+              .unroll(win.y, blockSize);
+
+            blur_unnormalized.compute_at(hw_output, xo);
+            blur.compute_at(hw_output, xo);
+
+            hw_input.stream_to_accelerator();//.compute_root();//at(output, xo);
+            //hw_input.compute_root();
           
-          //hw_input.compute_at(hw_output, xo);
-          //input_copy.stream_to_accelerator();
-          //input_copy.compute_root();
+            //hw_input.compute_at(hw_output, xo);
+            //input_copy.stream_to_accelerator();
+            //input_copy.compute_root();
+
+          }
           
         } else {    // schedule to CPU
 
-          /*output.tile(x, y, xo, yo, xi, yi, imgSize, imgSize)
+          /*output.tile(x, y, xo, yo, xi, yi, outputSize, outputSize)
             .vectorize(xi, 8)
             .fuse(xo, yo, xo)
             .parallel(xo);*/
