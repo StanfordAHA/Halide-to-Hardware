7b887899886bf28bd214d8c7e399ab8cc0f3c1d5
Jeff Setter <setter@stanford.edu>
2021-12-01 11:16:05 -0800

    Debug some bfloat stuff

--- apps/hardware_benchmarks/tests/fp_exp/fp_exp_generator.cpp
+++ apps/hardware_benchmarks/tests/fp_exp/fp_exp_generator.cpp
@@ -21,7 +21,7 @@ public:
         Func exponential, natural_log, power, square_root;
         exponential(x,y) = exp(hw_input(x,y));
         natural_log(x,y) = exponential(x,y) + log(hw_input(x,y));
-        power(x,y) = pow(natural_log(x,y), 3);
+        power(x,y) = pow(hw_input(x,y), 3) + natural_log(x,y);
         square_root(x,y) = sqrt(power(x,y) + 1);
 
         Func hw_output("hw_output");
