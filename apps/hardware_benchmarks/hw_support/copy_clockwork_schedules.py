3e56df1162562cc1a68f2457201fa3ab98ff3e4e
root <melchert@stanford.edu>
2022-08-23 11:44:42 -0700

    Added fix for new mem config

--- apps/hardware_benchmarks/hw_support/copy_clockwork_schedules.py
+++ apps/hardware_benchmarks/hw_support/copy_clockwork_schedules.py
@@ -4,7 +4,6 @@ fsource = open(sys.argv[1], "r")
 fdest = open(sys.argv[2], "r")
 flush_file = open(sys.argv[3], "r")
 pond_file = open(sys.argv[4], "r")
-
 source_lines = fsource.readlines()
 dest_lines = source_lines
 
@@ -12,8 +11,8 @@ new_schedules = {}
 new_pond_schedules = {}
 
 for idx, line in enumerate(source_lines):
-    if line.strip() == '"modref":"global.MEM",' or line.strip() == '"modref":"global.Pond",':
-        new_schedules[source_lines[idx-1]] = source_lines[idx+1]
+    if line.strip() == '"genref":"cgralib.Mem",' or line.strip() == '"modref":"global.Pond",':
+        new_schedules[source_lines[idx-1]] = source_lines[idx+3]
     if line.strip() == '"modref":"global.Pond",' and 'POND_PIPELINED' in os.environ and os.environ['POND_PIPELINED'] == '1':
         new_pond_schedules[source_lines[idx-1]] = source_lines[idx+1]
 
@@ -28,7 +27,7 @@ for idx, line in enumerate(dest_lines):
             def replace_func(matchobj):
                 return str(int(matchobj.group(1)) + flush_latencies[mem_name])
             new_line = re.sub(r'(?<=cycle_starting_addr":\[)(\d*)(?=],"cycle_stride)', replace_func, new_line)
-        dest_lines[idx+2] = new_line
+        dest_lines[idx+4] = new_line
     if line in new_pond_schedules:
         new_line = new_pond_schedules[line]
         pond_name = line.split('"')[1]
