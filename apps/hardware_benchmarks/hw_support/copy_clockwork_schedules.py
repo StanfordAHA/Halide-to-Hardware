fd618f1b9a361a3fe6eb430d85508044f97a0d05
root <melchert@stanford.edu>
2022-08-29 11:16:27 -0700

    Fixed pond bug

--- apps/hardware_benchmarks/hw_support/copy_clockwork_schedules.py
+++ apps/hardware_benchmarks/hw_support/copy_clockwork_schedules.py
@@ -7,21 +7,24 @@ pond_file = open(sys.argv[4], "r")
 source_lines = fsource.readlines()
 dest_lines = source_lines
 
-new_schedules = {}
+new_mem_schedules = {}
 new_pond_schedules = {}
+new_pond_pipe_schedules = {}
 
 for idx, line in enumerate(source_lines):
-    if line.strip() == '"genref":"cgralib.Mem",' or line.strip() == '"modref":"global.Pond",':
-        new_schedules[source_lines[idx-1]] = source_lines[idx+3]
-    if line.strip() == '"modref":"global.Pond",' and 'POND_PIPELINED' in os.environ and os.environ['POND_PIPELINED'] == '1':
+    if line.strip() == '"genref":"cgralib.Mem",':
+        new_mem_schedules[source_lines[idx-1]] = source_lines[idx+3]
+    if line.strip() == '"modref":"global.Pond",':
         new_pond_schedules[source_lines[idx-1]] = source_lines[idx+1]
+    if line.strip() == '"modref":"global.Pond",' and 'POND_PIPELINED' in os.environ and os.environ['POND_PIPELINED'] == '1':
+        new_pond_pipe_schedules[source_lines[idx-1]] = source_lines[idx+1]
 
 flush_latencies = json.load(flush_file)
 pond_latencies = json.load(pond_file)
 
 for idx, line in enumerate(dest_lines):
-    if line in new_schedules:
-        new_line = new_schedules[line]
+    if line in new_mem_schedules:
+        new_line = new_mem_schedules[line]
         mem_name = line.split('"')[1]
         if mem_name in flush_latencies:
             def replace_func(matchobj):
@@ -30,6 +33,14 @@ for idx, line in enumerate(dest_lines):
         dest_lines[idx+4] = new_line
     if line in new_pond_schedules:
         new_line = new_pond_schedules[line]
+        mem_name = line.split('"')[1]
+        if mem_name in flush_latencies:
+            def replace_func(matchobj):
+                return str(int(matchobj.group(1)) + flush_latencies[mem_name])
+            new_line = re.sub(r'(?<=cycle_starting_addr":\[)(\d*)(?=],"cycle_stride)', replace_func, new_line)
+        dest_lines[idx+2] = new_line
+    if line in new_pond_pipe_schedules:
+        new_line = new_pond_pipe_schedules[line]
         pond_name = line.split('"')[1]
         if pond_name in pond_latencies:
             def replace_func(matchobj):
