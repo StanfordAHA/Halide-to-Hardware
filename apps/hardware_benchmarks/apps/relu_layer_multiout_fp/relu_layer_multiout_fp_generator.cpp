e6c6f497b15975633784cb25806a372a3586345a
yuchen-mei <yuchen.mei@outlook.com>
2025-09-12 17:43:44 -0700

    add a unit test for multi-output CGRA app

---
+++ apps/hardware_benchmarks/apps/relu_layer_multiout_fp/relu_layer_multiout_fp_generator.cpp
@@ -0,0 +1,75 @@
+#include "Halide.h"
+
+namespace {
+
+using namespace std;
+
+using namespace Halide;
+using namespace Halide::ConciseCasts;
+
+class ReluLayer : public Halide::Generator<ReluLayer> {
+public:
+    Input<Buffer<uint16_t>> input{ "input", 3 };
+    Input<Buffer<uint16_t>> bias{ "bias", 3 };
+    Output<Buffer<uint16_t>> output{ "output", 3 };
+
+    GeneratorParam<int> out_img{ "out_img", 28 };
+    GeneratorParam<int> n_oc{ "n_oc", 8 };
+
+    GeneratorParam<int> unroll{ "unroll", 4 };
+
+    void generate() {
+        /* THE ALGORITHM */
+
+        /* This app serves as a unit test for a multi-output CGRA call.
+        It contains two outputs: hw_output for bias+relu, and hw_add_3 for input+3.
+        All additional outputs are added in process.cpp and hack_design_top.py instead of here. */
+
+        Var x("x"), y("y"), w("w");
+        Func hw_input("hw_input"), input_host("input_host"), input_glb("input_glb"), input_cgra("input_cgra");
+        Func hw_bias("hw_bias"), bias_host("bias_host"), bias_glb("bias_glb"), bias_cgra("bias_cgra");
+        Func hw_output("hw_output"), output_glb("output_glb"), output_cgra("output_cgra");
+        Func relu;
+
+        hw_input(w, x, y) = cast<bfloat16_t>(input(w, x, y));
+        hw_bias(w, x, y) = cast<bfloat16_t>(bias(w, x, y));
+
+        relu(w, x, y) = max(hw_input(w, x, y) + hw_bias(w, x, y), cast<bfloat16_t>(0.0f));
+
+        hw_output(w, x, y) = relu(w, x, y);
+        output(w, x, y) = cast<uint16_t>(hw_output(w, x, y));
+
+        /* THE SCHEDULE */
+        if (get_target().has_feature(Target::CoreIR) ||
+            get_target().has_feature(Target::HLS)) {
+
+        } else if (get_target().has_feature(Target::Clockwork)) {
+            Var xi, yi, xo, yo;
+
+            output.bound(x, 0, out_img);
+            output.bound(y, 0, out_img);
+            output.bound(w, 0, n_oc);
+
+            //hw_input.compute_root();
+            hw_output.compute_root();
+            hw_output
+                .tile(x, y, xo, yo, xi, yi, out_img, out_img)
+                .reorder(w, xi, yi, xo, yo)
+                .hw_accelerate(xi, xo);
+            hw_output.unroll(w, unroll);
+
+            relu.compute_at(hw_output, xo).unroll(w, unroll);
+
+            hw_input.stream_to_accelerator();
+            hw_input.in().unroll(w, unroll);
+
+            hw_bias.stream_to_accelerator();
+            hw_bias.in().unroll(w, unroll);
+        } else {
+        }
+    }
+};
+
+}  // namespace
+
+HALIDE_REGISTER_GENERATOR(ReluLayer, relu_layer_multiout_fp)
